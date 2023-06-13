8b81b0ae6c124e50396641fb6e2b164b8eb4ca02
Mtorres86 <iata24@outlook.es>
2023-06-13 21:14:00 +0200

    Se subio por primera vez

---
+++ src/main/java/hn/mtech/calcular_areas/App.java
@@ -0,0 +1,13 @@
+package hn.mtech.calcular_areas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
